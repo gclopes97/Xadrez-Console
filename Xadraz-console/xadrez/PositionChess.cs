7579d6b520f87c3196c92c02ed2cc49a965f840a
Gilberto Lopes <gclopes97@hotmail.com>
2020-08-11 16:56:57 -0300

    Criando a classe Posicao Xadrez

---
+++ Xadraz-console/xadrez/PositionChess.cs
@@ -0,0 +1,18 @@
+﻿using tabuleiro;
+
+namespace Xadraz_console.xadrez {
+    class PositionChess {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+        public PositionChess(char coluna, int linha) {
+            Coluna = coluna;
+            Linha = linha;
+        }
+        public Position ToPosition() {
+            return new Position(8 - Linha, Coluna - 'a');
+        }
+        public override string ToString() {
+            return "" + Coluna + Linha;
+        }
+    }
+}
